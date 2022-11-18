29df359e853cd9af4c753b251dc4ba11cb8bc06a
VexheslavSP <willy0@rambler.ru>
2022-11-18 15:20:27 +0300

    Finish second variant

--- Task_50/Task_50.cs
+++ Task_50/Task_50.cs
@@ -1,8 +1,8 @@
-// вариант поиска по двум индексам
+// вариант поиска индекса по значению числа
 byte[,] BuildArray()
 {
   var rnd =new Random();
-  byte tmpSqear=(byte)rnd.Next(1,byte.MaxValue); //здесь и далее. создается квадратный массив. понятно что прямоугольный будет работать так же. Варианты пилообразных массивов (1 строка 5 столбцов,2 строка 20 столбцов, 3 строка 2 столбца и  т.д.) в данной задаче не рассматриваются
+  byte tmpSqear=(byte)rnd.Next(2,byte.MaxValue); // минимальный квадрат 2х2. иначе одномерный массив
   byte[,] myArray=new byte[tmpSqear,tmpSqear];
   for (byte i=0;i<tmpSqear;i++)
   {
@@ -13,49 +13,34 @@ byte[,] BuildArray()
   }
   return myArray;
 }
-byte [] CheckArrayRowAndCollum(string message)
+void SreachPozition(byte[,] myArray, byte number)
 {
-  int length=message.Length;
-  byte[] arrayRowAndCollum= new byte [3]; // третья цифра зарезервирована для проверки корректности. т.к. размерность byte -1 использовать нельзя, поэтому 0=> все нормально, byte.MaxValue=> ошибка
-  try
+  try{
+    for (int i=0; i<=myArray.GetLength(1); i++) {
+      for (int j=0; j<=myArray.GetLength(0); j++) {
+        if (myArray[i,j]==number) {
+          Console.WriteLine("Индексы числа {"+i+","+j+"}");
+          return;
+        }
+      }
+    }
+  }
+  catch
   {
-    int pointPozition=message.IndexOf(',');
-    if(pointPozition==-1)
-    {
-      throw new Exception("неккорекный ввод, не найден разделитель");
-    }
-    try
-    {
-      byte row=Convert.ToByte(message.Substring(0,pointPozition));
-      byte collum=Convert.ToByte(message.Substring(pointPozition+1));
-      arrayRowAndCollum[0]=row;
-      arrayRowAndCollum[1]=collum;
-    }
-    catch {
-      throw new Exception("неккорекный ввод индексов");
-    }
+    Console.WriteLine("такого числа в массиве нет");
   }
-  catch (Exception e)
-  {
-    Console.WriteLine($"Ошибка: {e.Message}");
-    arrayRowAndCollum[2]=byte.MaxValue;
-  }
-return   arrayRowAndCollum;
-}
-void Result (byte[,] myArray,byte[] arrayRowAndCollum)
-{
-  try {
-    Console.WriteLine("ответ число "+ myArray[arrayRowAndCollum[0],arrayRowAndCollum[1]]);
-  }
-  catch  {
-    Console.WriteLine("Числа с таким индексом в массиве нет");
-  }
-  return;
 }
 byte[,]myArray=BuildArray();
-//byte[,]myArray=new byte[,]{{1,4,7,2},{5,9,2,3},{8,4,2,4}};
-Console.WriteLine("Введите индексы(не более 255) числа через разделитель ','");
-byte[] arrayRowAndCollum=CheckArrayRowAndCollum(Console.ReadLine());
-if (arrayRowAndCollum[2]==0) {
-  Result(myArray,arrayRowAndCollum);
+//byte[,]myArray=new byte[,]{{1,4,7,123},{5,9,2,3},{8,4,140,255}};
+byte number;
+while (true) {
+  Console.WriteLine("Введите искомое число от 0 до 255");
+  try {
+    number=Convert.ToByte(Console.ReadLine());
+    break;
+  }
+  catch {
+    Console.WriteLine("Ошибка: неккорекный ввод числа");
+  }
 }
+SreachPozition(myArray,number);
